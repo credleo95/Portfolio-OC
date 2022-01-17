5ba7f612e19a1b4c851a867d3d8114cfb9282270
Omarius <70237021+credleo95@users.noreply.github.com>
2022-01-17 08:35:45 -0500

    updated email text display

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,7 +25,7 @@ const Footer = () => {
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
           <LinkItem href="mailto:contact@omarius.dev">
-            credleo@outlook.com
+            contact@omarius.dev
           </LinkItem>
         </LinkColumn>
       </LinkList>
